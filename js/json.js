cd76cc1a0d62c644cd20c06f4684fb281980095a
Michael Frohberg <michael.frohberg@gmail.com>
2013-05-15 11:59:50 -0400

    Lots
    Created local json
    Created "remote" json
    jquery functions

---
+++ js/json.js
@@ -0,0 +1,58 @@
+var json = {
+	"gig 1": {
+		"paid": ["Paid:", "yes"],
+		"kind": ["Type of Gig:", "event" ],
+		"area": ["Area:", "brooklyn"],
+		"date": ["Start Date:", "4/21/2012"],
+		"post": ["Post Title:", "Shakespeare" ],
+		"details": ["Details:", "To be, or not to be, that is the question" ],
+		"cName": ["Contact Name:", "Michael"],
+		"email": ["Contact Email:", "michaelfrohberg@me.com"],
+		"phone": ["Phone:", "347-525-4218"]
+    },
+    "gig 2": {
+    	"paid": ["Paid:", "no"],
+		"kind": ["Type of Gig:", "talent"],
+		"area": ["Area:", "bronx"],
+		"date": ["Start Date:", "4/20/2012"],
+		"post": ["Post Title:", "Weed"],
+		"details": ["Details:", "To be, or not to be, that is the question" ],
+		"cName": ["Contact Name:", "Bernie Biggs"],
+		"email": ["Contact Email:", "berniebiggs@me.com"],
+		"phone": ["Phone:", "213-525-4218"]
+	},
+	"gig 3": {
+		"paid": ["Paid:", "yes"],
+		"kind": ["Type of Gig:", "writing"],
+		"area": ["Area:", "manhattan"],
+		"date": ["Start Date:", "3/12/2013"],
+		"post": ["Post Title:", "Kittens"],
+		"details": ["Details:", "To be, or not to be, that is the question" ],
+		"cName": ["Contact Name:", "Michelle Anton"],
+		"email": ["Contact Email:", "mookie@me.com"],
+		"phone": ["Phone:", "646-525-2134"]
+    },
+    "gig 4": {
+	    "paid": ["Paid:", "yes"],
+		"kind": ["Type of Gig:", "writing"],
+		"area": ["Area:", "manhattan"],
+		"date": ["Start Date:", "3/12/2013"],
+		"post": ["Post Title:", "Kittens"],
+		"details": ["Details:", "To be, or not to be, that is the question" ],
+		"cName": ["Contact Name:", "Michelle Anton"],
+		"email": ["Contact Email:", "mookie@me.com"],
+		"phone": ["Phone:", "646-525-2134"],
+    },
+	"gig 5": {
+	    "paid": ["Paid:", "no"],
+		"kind": ["Type of Gig:", "creative"],
+		"area": ["Area:", "87"],
+		"date": ["Start Date:", "4/20/2012"],
+		"post": ["Post Title:", "Heyyyyyy"],
+		"details": ["Details:", "To be, or not to be, that is the question" ],
+		"cName": ["Contact Name:", "Sven"],
+		"email": ["Contact Email:", "michaelfrohberg@me.com"],
+		"phone": ["Phone:", "347-525-4218"],
+
+	 }
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,68 @@
-$('#browse').on('pageinit', function() {
-	$('#submit').on('click', function(event) {
-		event.preventDefault();
-		// Act on the event
-	});
-})
-$('#postGig').on('pageinit', function() {
-	$('li').on('click', function(e) {
-        alert($.this.html());
-        e.stopPropagation();
-		// Act on the event
-	});
+
+
+
+
+$("#addItem").on("pageinit",  function(){
+	var myForm = $('#gig-form'); // creates a jquery object.
+/*  	myForm.validate({
+      invalidHandler: function (myForm, validator) {
+          var errors = validator.numberOfInvalids();
+          if (errors) {
+              var message = errors == 1 ? "You missed 1 field. It has been highlighted"
+              : "You missed " + errors + " fields. They have been highlighted";
+              $('div.error span').html(message).appendTo('.content');
+              $('div.error').show();
+          } else {
+              $('div.error').hide();
+          }
+      },
+      // if no errors return pure form
+      submitHandler: function () {
+      	console.log("Valid Code You may Proceed");
+	     });
+    }); 
+*/
+    function storeLocal(key ) {
+      if (!key) {
+        var id = Math.floor(Math.random()*1000001);
+      } else { 
+        id = key;
+      }
+      var item = {}
+        item.paid = 	["Paid:", $("#paid").val()]; 
+        item.kind = 	["Type", $("#gig-select").val()]; 
+        item.area = 	["Area", $("input:checked").val()];
+        item.date = 	["Date", $("#date").val()];
+        item.post = 	["Post Title", $("#post").val()];
+        item.details = 	["Details", $("#details").val()];
+        item.cName =	["Contact Name:", $("#name").val()];
+        item.email = 	["Contact Email:", $("#email").val()];
+        item.phone = 	["Conact Phone:", $("#phone").val()];
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Post Saved to local storage");
+    }
+    function displayLocal() {
+      if (localStorage.length == 0) {
+        alert("You have nothing saved.")
+      } else {
+        alert(JSON.parse(localStorage.item))
+      }
+
+    }
+    // Events
+    $("#submit").on("click", function (e) {
+        e.preventDefault()
+        storeLocal();
+    });
+    $("#display").on("click", function(e) {
+        e.preventDefault();
+        displayLocal();
+    });
+
 });
+
+
+  		
+  	
+ 
+
